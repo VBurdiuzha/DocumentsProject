11da6a97c0a49bb31f5dc339528d38fdfd9b9033
Вилли Бурдюжа <villiburduza@MacBook-Pro-Villi.local>
2018-05-10 12:29:25 +0300

    add xml

--- src/test/java/uiTest/LogIn.java
+++ src/test/java/uiTest/LogIn.java
@@ -6,23 +6,24 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 
 
-public class classTest extends WebDriverTestBase {
+public class LogIn extends WebDriverTestBase {
 
 
     private LoginPage loginPage;
 
     @BeforeClass
     public void initPages() {
+        loginPage = PageFactory.initElements(browser, LoginPage.class);
         System.out.println("Jira Pages Initialized");
     }
 
-    @Test(description = "Invalid Login", priority = -1)
+    @Test (description = "Invalid Login", priority = -1, groups = {"LogIn"})
     public void failureLogin() {
 
         loginPage.failureLogin();
     }
 
-    @Test(description = "Valid Login")
+    @Test  (description = "Valid Login", groups = {"LogIn"})
     public void successfulLogin() {
 
         loginPage.successfulLogin();
