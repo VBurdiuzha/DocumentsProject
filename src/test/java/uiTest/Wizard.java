6da34f2bf48ddda850023c080f086bfc9e16bb2f
Myza Taras <taras.myza@gmail.com>
2018-06-07 17:22:53 +0300

    fix

--- src/test/java/uiTest/Wizard.java
+++ src/test/java/uiTest/Wizard.java
@@ -1,6 +1,6 @@
 package uiTest;
 
-import mainPackage.Vars;
+import mainPackage.interfaceFolder.Vars;
 import mainPackage.ui.DocCategory;
 import mainPackage.ui.LoginPage;
 import mainPackage.utils.WebDriverTestBase;
