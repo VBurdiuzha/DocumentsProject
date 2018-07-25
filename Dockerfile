e56682119c82478168ca01d2bc719ea3095f426e
Вилли Бурдюжа <villiburduza@MacBook-Pro-Villi.local>
2018-07-25 17:08:36 +0300

    ctr+z cross platform

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+FROM java:8
 FROM selenium/standalone-chrome
 MAINTAINER  Burdiuzha Villi <v.burdiuzha@ossystem.com.ua>
 WORKDIR /var/www/tests
@@ -18,21 +19,6 @@ RUN echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /
     apt-get install -y --force-yes --no-install-recommends oracle-java${JAVA_VER}-installer oracle-java${JAVA_VER}-set-default && \
     apt-get clean && \
     rm -rf /var/cache/oracle-jdk${JAVA_VER}-installer
-
-
-RUN apt-get install -y xvfb
-
-RUN apt-get update
-RUN sudo apt-get  install -y xvfb x11vnc x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
-
-ADD xvfb_init /var/www/tests/xvfb/init.d/xvfb
-RUN chmod a+x /var/www/tests/xvfb/init.d/xvfb
-ADD xvfb_daemon_run /usr/bin/xvfb-daemon-run
-RUN chmod a+x /usr/bin/xvfb-daemon-run
-
-ENV DISPLAY :99
-
-
 
 #RUN apt-cache search chrome browser
 #RUN apt-get install chrome-browser
--- src/main/java/mainPackage/utils/WebDriverTestBase.java
+++ src/main/java/mainPackage/utils/WebDriverTestBase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,24 +16,24 @@ public class WebDriverTestBase {
         System.setProperty("webdriver.chrome.driver", "/var/www/tests/src/main/resources/selenium/linux/chromedriver");
     }
 
- //   @Parameters ("platform")
+    @Parameters("platform")
     @BeforeTest
-    public static void setUp(/*String platform*/) {
-//        if ("mac".equals(platform)) {
-//            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/mac/chromedriver");
-//            System.out.println("Test running on mac os");
-//
-//    } else if ("linux".equals(platform)) {
-//            System.setProperty("webdriver.chrome.driver", "/var/www/tests/src/main/resources/selenium/linux/chromedriver");
-//            System.out.println("Test running on linux os");
-//
-//        } else if ("windows".equals(platform)) {
-//            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/windows/chromedriver.exe");
-//            System.out.println("Test running on windows");
-//
-//        } else {
-//            System.out.println("Something wrong ! :(");
-//        }
+    public static void setUp(String platform) {
+        if ("mac".equals(platform)) {
+            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/mac/chromedriver");
+            System.out.println("Test running on mac os");
+
+    } else if ("linux".equals(platform)) {
+            System.setProperty("webdriver.chrome.driver", "/var/www/tests/src/main/resources/selenium/linux/chromedriver");
+            System.out.println("Test running on linux os");
+
+        } else if ("windows".equals(platform)) {
+            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/windows/chromedriver.exe");
+            System.out.println("Test running on windows");
+
+        } else {
+            System.out.println("Something wrong ! :(");
+        }
 
 
 
@@ -41,6 +42,9 @@ public class WebDriverTestBase {
             driver.manage().window().maximize();
             mainPackage.utils.WebDriverTools.setDriver(driver);
 
+        ChromeOptions ChromeOptions = new ChromeOptions();
+        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+        driver = new ChromeDriver(ChromeOptions);
     }
 
 
