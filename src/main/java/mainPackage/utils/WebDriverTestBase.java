f502601498de17459a743c7b903a83b4f561999e
Вилли Бурдюжа <villiburduza@MacBook-Pro-Villi.local>
2018-07-26 10:21:25 +0300

    set DriverTestBase to headless chrome

--- src/main/java/mainPackage/utils/WebDriverTestBase.java
+++ src/main/java/mainPackage/utils/WebDriverTestBase.java
@@ -1,50 +1,58 @@
 package mainPackage.utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import java.util.concurrent.TimeUnit;
-
 public class WebDriverTestBase {
     protected static WebDriver driver;
 
     {
-        System.setProperty("webdriver.chrome.driver", "/var/www/tests/src/main/resources/selenium/linux/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/selenium/linux/chromedriver");
     }
 
     @Parameters("platform")
     @BeforeTest
     public static void setUp(String platform) {
-        if ("mac".equals(platform)) {
-            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/mac/chromedriver");
-            System.out.println("Test running on mac os");
+//        if ("mac".equals(platform)) {
+//            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/mac/chromedriver");
+//            System.out.println("Test running on mac os");
+//
+//    } else if ("linux".equals(platform)) {
+//            System.setProperty("webdriver.chrome.driver", "./src/main/resources/selenium/linux/chromedriver");
+//            System.out.println("Test running on linux os");
+//
+//        } else if ("windows".equals(platform)) {
+//            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/windows/chromedriver.exe");
+//            System.out.println("Test running on windows");
+//
+//        } else {
+//            System.out.println("Something wrong ! :(");
+//        }
 
-    } else if ("linux".equals(platform)) {
-            System.setProperty("webdriver.chrome.driver", "./src/main/resources/selenium/linux/chromedriver");
-            System.out.println("Test running on linux os");
-
-        } else if ("windows".equals(platform)) {
-            System.setProperty("webdriver.chrome.driver", "/Users/villiburduza/IdeaProjects/ServiceDoc/qa/src/main/resources/selenium/windows/chromedriver.exe");
-            System.out.println("Test running on windows");
-
-        } else {
-            System.out.println("Something wrong ! :(");
-        }
 
 
+//            driver = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized", "--incognito"));
+//            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+//            driver.manage().window().maximize();
+//            mainPackage.utils.WebDriverTools.setDriver(driver);
+//
+//        ChromeOptions ChromeOptions = new ChromeOptions();
+//        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+//        driver = new ChromeDriver(ChromeOptions);
 
-            driver = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized", "--incognito"));
-            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-            driver.manage().window().maximize();
-            mainPackage.utils.WebDriverTools.setDriver(driver);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+// Must maximize Chrome by `start-maximized`
+        options.addArguments("start-maximized");
 
-        ChromeOptions ChromeOptions = new ChromeOptions();
-        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-        driver = new ChromeDriver(ChromeOptions);
+//Dont maximize Chrome by below line, because has no display
+        driver.manage().window().maximize();
+
+
+
     }
 
 
