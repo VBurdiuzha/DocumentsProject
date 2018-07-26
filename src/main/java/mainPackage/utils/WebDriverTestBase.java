00a171855a3fd8b9d8063acf1082d5260c813580
Вилли Бурдюжа <villiburduza@MacBook-Pro-Villi.local>
2018-07-26 15:03:03 +0300

    -__-

--- src/main/java/mainPackage/utils/WebDriverTestBase.java
+++ src/main/java/mainPackage/utils/WebDriverTestBase.java
@@ -57,9 +57,6 @@ public class WebDriverTestBase {
         options.addArguments("--disable-dev-shm-usage");
 
 
-// Must maximize Chrome by `start-maximized`
-       // options.addArguments("start-maximized");
-
 //Dont maximize Chrome by below line, because has no display
         driver.manage().window().maximize();
 
