d895a8a40d35d8cf94c392d51fa8f04f4f16e225
Вилли Бурдюжа <villiburduza@MacBook-Pro-Villi.local>
2018-08-17 12:01:42 +0300

    delete unnecessary folder

--- ,/classes/bash/setFullRunSelenium.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cat FullRun.xml | sed 's/<parameter name=\"selenium\" value=\"local\" \/>/<parameter name=\"selenium\" value=\"remove\" \/>/g' > out.xml && mv out.xml FullRun.xml
--- ,/classes/bash/setPom.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cat pom.xml | sed 's/<suiteXmlFile>.*<\/suiteXmlFile>/<suiteXmlFile>src\/main\/resources\/xml\/FullRun.xml<\/suiteXmlFile>/' > out.xml && rm pom.xml && mv out.xml pom.xml
